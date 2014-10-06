b5ff7715710151fe7c7323bb9d45eceb4f0d691f
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-10-06 19:48:33 -0400

    Added validation and flash messages; converted Cancel back to link instead of button since it wasn't working properly with validation

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,9 @@
 class Wine < ActiveRecord::Base
+  validates :name, :country, :year, presence: true
+  validates :year,
+    numericality: {only_integer: true},
+    if: "year.present?"
+  validates :varietal, inclusion: { in: %w(Merlot Barbera Chardonnay),
+    message: "%{value} is not a valid wine varietal." },
+    unless: "varietal.blank?"
 end
