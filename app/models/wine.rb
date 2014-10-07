0476413d682c6482843d24da22fec38690e3a18f
Lisa Lowe <lisa.s.lowe@gmail.com>
2014-10-06 20:18:49 -0400

    Added select for varietals

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,9 +1,10 @@
 class Wine < ActiveRecord::Base
+  VARIETALS = ["Merlot", "Barbera", "Chardonnay"]
   validates :name, :country, :year, presence: true
   validates :year,
     numericality: {only_integer: true},
     if: "year.present?"
-  validates :varietal, inclusion: { in: %w(Merlot Barbera Chardonnay),
+  validates :varietal, inclusion: { in: VARIETALS,
     message: "%{value} is not a valid wine varietal." },
     unless: "varietal.blank?"
 end
